be595687238473d98ef9fd63bc5bf05424e04167
yonB280405 <giabao2005bbb@gmail.com>
2024-07-02 22:11:06 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
